3c3fc2af5f6de67260d4b9f1b78e3a11319de069
Heesung Shin <heesung.shin@fdmgroup.com>
2023-03-03 12:10:08 -0500

    fourth commit added providers.tf

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.56.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+}
